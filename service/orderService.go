74a4227b25855a1a771214639ba6cc1fb70a9c55
Denzil Pinto <denzilpinto01@gmail.com>
2026-08-14 08:05:57 +0530

    merged both validate and calculate funtions into one

--- service/orderService.go
+++ service/orderService.go
@@ -15,11 +15,16 @@ func NewOrderService(db *gorm.DB) *OrderService {
 	return &OrderService{DB: db}
 }
 
-func (os *OrderService) ValidateCart(items []models.CartlineInput) ([]models.Product, error) {
-	var products []models.Product
-	for i, lineProduct := range items {
-		fmt.Printf("I : %v\n", i)
-		fmt.Printf("lineProduct : %v\n", lineProduct)
+// FIX : normal unit - int others - float64
+type ValidProduct struct {
+	Product  models.Product
+	Quantity float64
+}
+
+func (os *OrderService) ValidateCart(items []models.CartlineInput) ([]ValidProduct, error) {
+	var validProducts []ValidProduct
+	var totalBill float64
+	for _, lineProduct := range items {
 		var product models.Product
 		res := os.DB.First(&product, lineProduct.ProductID)
 		if errors.Is(res.Error, gorm.ErrRecordNotFound) {
@@ -27,25 +32,17 @@ func (os *OrderService) ValidateCart(items []models.CartlineInput) ([]models.Pro
 			// so if one invalid product is in the cart line whole cehck out should cancel and restart
 			return nil, fmt.Errorf("product %v is no longer available", lineProduct.ProductID)
 		}
-		products = append(products, product)
-	}
-	return products, nil
-}
-
-func (os *OrderService) CalculateTotal(items []models.CartlineInput) (float64, error) {
-	var total float64
-	for _, lItem := range items {
-		var product models.Product
-		res := os.DB.First(&product, lItem.ProductID)
-		if errors.Is(res.Error, gorm.ErrRecordNotFound) {
-			return 0.0, fmt.Errorf("product %v is no longer available", lItem.ProductID)
+		vp := ValidProduct{
+			Product:  product,
+			Quantity: float64(lineProduct.Quantity),
 		}
-		// current product per unit price
-		oneUnitPrice := product.Price
-		fmt.Println("Current per unit price : ", oneUnitPrice)
-		fmt.Println("Last total : ", total)
-		total += oneUnitPrice * float64(lItem.Quantity)
-	}
-	return total, nil
+		validProducts = append(validProducts, vp)
 
+		// calculate total per unit
+		// unit depends on the product
+		oneUnitPrice := product.Price
+		totalBill += oneUnitPrice * float64(lineProduct.Quantity)
+
+	}
+	return validProducts, nil
 }
