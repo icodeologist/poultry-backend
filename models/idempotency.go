d523776fff818f0324dc7228a6971bc93b19a57b
Denzil Pinto <denzilpinto01@gmail.com>
2026-08-19 19:37:59 +0530

    both record payment and make order can pass through middleware with single type

--- models/idempotency.go
+++ models/idempotency.go
@@ -4,17 +4,9 @@ import (
 	"time"
 )
 
-type OrderIdempotency struct {
+type OrderAndPaymentIdempotency struct {
 	ID             uint   `gorm:"primaryKey"`
 	Key            string `gorm:"unique"`
-	RequestHash    string
-	ResponseBody   string
-	ResponseStatus int
-	CreatedAt      time.Time
-}
-
-type PaymentIdempotency struct {
-	Key            string `gorm:"primaryKey"`
 	RequestHash    string
 	ResponseBody   string
 	ResponseStatus int
