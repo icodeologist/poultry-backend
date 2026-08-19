07d638f92c22f6690c238650589623a2e56f6ace
Denzil Pinto <denzilpinto01@gmail.com>
2026-08-19 13:59:25 +0530

    tested&added: idempotency middleware;add response recorder + unique index model to stop duplicate requests

--- middleware/idempotency.go
+++ middleware/idempotency.go
@@ -3,71 +3,71 @@ package middleware
 import (
 	"errors"
 	"fmt"
+	"log"
 	"net/http"
+	"net/http/httptest"
 	"time"
 
-	"github.com/icodeologist/poultry-backend/db"
 	"github.com/icodeologist/poultry-backend/models"
 	"gorm.io/gorm"
 )
 
-// func Middleware(next http.Handler) http.Handler {
-// 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-// 		log.Println("passing through middleware")
-// 		// next.ServeHTTP(w, r)
-// 	})
-//
-// }
-
-func IdempotencyMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodPost {
-			next.ServeHTTP(w, r)
-			return
-		}
-
-		key := r.Header.Get("Idempotency-Key")
-		var paymentImp models.PaymentIdempotency
-
-		res := db.DB.Where("key=?", key).First(&paymentImp)
-		if errors.Is(res.Error, gorm.ErrRecordNotFound) {
-			// record does not exist
-			pi := models.PaymentIdempotency{
-				Key:            key,
-				ResponseBody:   "",
-				ResponseStatus: 0,
-				CreatedAt:      time.Now(),
-			}
-			if err := db.DB.Create(&pi).Error; err != nil {
-				http.Error(w, "could not claim the key", http.StatusInternalServerError)
+func IdempotencyMiddleware(db *gorm.DB) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if r.Method != http.MethodPost {
+				log.Println("Method is not post")
+				http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
 				return
 			}
-			rec := &responseRecorder{ResponseWriter: w}
-			next.ServeHTTP(rec, r)
-
-			db.DB.Model(&pi).Updates(models.PaymentIdempotency{
-				ResponseStatus: rec.Code,
-				ResponseBody:   rec.Body,
-			})
-
-		} else if res.Error != nil {
-			http.Error(w, fmt.Sprint(res.Error), http.StatusInternalServerError)
-			return
-		} else {
-			if paymentImp.ResponseStatus == 0 {
-				http.Error(w, `{"error": "request already in progress, try again shortly"}`, http.StatusConflict)
+			key := r.Header.Get("Idempotency-Key")
+			log.Println("KEY : ", key)
+			if key == "" {
+				http.Error(w, "key is empty. Unauthorized", http.StatusUnauthorized)
+				// may be check if the key is correct here
+				// liek the format or something
+				// we will do that once we have key format set in front end
 				return
 			}
-			w.WriteHeader(paymentImp.ResponseStatus)
-			w.Write([]byte(paymentImp.ResponseBody))
-			return
-		}
-
-	})
-}
-
-type responseRecorder struct {
-	http.ResponseWriter
-	Code int
-	Body string
+			// now we assume key is valid
+			// is this new key or do we already have registered this key
+			// fecth the key data from db and if it exists then we have already processing or processed this request
+			var ip models.OrderIdempotency
+			ip.Key = key
+			ip.CreatedAt = time.Now()
+			err := db.Create(&ip).Error
+			// one ip is already registed with this key
+			// so reject this but dont send error send the status code and staus response from the original request
+			if errors.Is(err, gorm.ErrDuplicatedKey) {
+				log.Printf("Duplicated key reqeust : %v\n", err)
+				var existing models.OrderIdempotency
+				res := db.Where("key=?", key).First(&existing)
+				// no chnace of having no keys since we already know it exists with the key
+				if res.Error != nil {
+					http.Error(w, res.Error.Error(), http.StatusInternalServerError)
+					return
+				} else {
+					if existing.ResponseStatus == 0 {
+						fmt.Fprintf(w, fmt.Sprintf("Request with key :%v is still processing.\n", key))
+						return
+					}
+					w.WriteHeader(existing.ResponseStatus)
+					w.Write([]byte(existing.ResponseBody))
+					return
+				}
+			} else if err != nil {
+				log.Printf("Error server side while creating Idempotency-Key : %v\n", err)
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			} else {
+				rr := httptest.NewRecorder()
+				next.ServeHTTP(rr, r)
+				ip.ResponseStatus = rr.Code
+				ip.ResponseBody = rr.Body.String()
+				db.Save(&ip)
+				w.WriteHeader(ip.ResponseStatus)
+				w.Write([]byte(ip.ResponseBody))
+			}
+		})
+	}
 }
