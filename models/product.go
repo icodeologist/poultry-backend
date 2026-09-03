c762b5496054fac1ba809860117139d403f7ab29
Denzil Pinto <denzilpinto01@gmail.com>
2026-09-03 20:53:48 +0530

    refactor: product model

--- models/product.go
+++ models/product.go
@@ -2,9 +2,9 @@ package models
 
 type Product struct {
 	ID             uint    `gorm:"primaryKey" json:"id"`
-	Title          string  `gorm:"not null" json:"title"`
-	Price          float64 `gorm:"not null" json:"price"`
-	Unit           string  `gorm:"size:20" json:"unit"` // kg piece or litre pack
+	Title          string  `json:"title"`
+	Price          float64 `json:"price"`
+	Unit           string  `json:"unit"` // kg piece or litre pack
 	TaxRate        float64 `json:"taxRate"`
 	Stock_Quantity int     `gorm:"stock_quantity" json:"stockQuantity"`
 }
